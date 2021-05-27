38858b0259eafd314ea521ff401c2576c64bca93
AndresTorrico09 <andreslionel09@outlook.com>
2021-05-27 11:56:39 -0300

    fix tests after merge

--- app/src/test/java/com/distillery/interview/ui/CurrentWeatherViewModelTests.kt
+++ app/src/test/java/com/distillery/interview/ui/CurrentWeatherViewModelTests.kt
@@ -8,7 +8,6 @@ import com.distillery.interview.data.models.WeatherResponse
 import com.distillery.interview.util.getOrAwaitValue
 import com.distillery.interview.util.provideFakeCoroutinesDispatcherProvider
 import com.distillery.interview.ui.current_weather.CurrentWeatherViewModel
-import com.distillery.interview.util.Event
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -41,20 +40,18 @@ class CurrentWeatherViewModelTests {
         //GIVEN: I want to know what is the current weather
         val mockWeatherResponse = mock<WeatherResponse>()
 
-        val expected = CurrentWeatherViewModel.CurrentWeatherUiModel(
-            showLoading = false,
-            showError = null,
-            showSuccess = Event(mockWeatherResponse),
-        )
-
-        //WHEN: I open my app or move to current weather screen
         whenever(weatherRepository.getCurrentWeather()).thenReturn(
             Result.Success(mockWeatherResponse)
         )
+
+        //WHEN: I open my app or move to current weather screen
         viewModel.getCurrentWeather()
 
         //THEN: I should to get current weather successfully
-        Assert.assertEquals(viewModel.uiState.getOrAwaitValue(), expected)
+        Assert.assertEquals(
+            viewModel.uiState.getOrAwaitValue(),
+            Result.Success(mockWeatherResponse)
+        )
     }
 
     @Test
@@ -62,20 +59,15 @@ class CurrentWeatherViewModelTests {
         //GIVEN: I want to know what is the current weather
         val mockWeatherResponse = listOf("errorResponseMessage")
 
-        //WHEN: I open my app or move to current weather screen
         whenever(weatherRepository.getCurrentWeather()).thenReturn(
             Result.Error(mockWeatherResponse)
         )
+
+        //WHEN: I open my app or move to current weather screen
         viewModel.getCurrentWeather()
 
         //THEN: I should to get current weather unsuccessfully
-        val expected = CurrentWeatherViewModel.CurrentWeatherUiModel(
-            showLoading = false,
-            showError = Event(mockWeatherResponse.first()),
-            showSuccess = null,
-        )
-
-        Assert.assertEquals(viewModel.uiState.getOrAwaitValue(), expected)
+        Assert.assertEquals(viewModel.uiState.getOrAwaitValue(), Result.Error(mockWeatherResponse))
 
     }
 }
