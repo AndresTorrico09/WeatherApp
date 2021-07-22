b0e46cc7486600d1a9e66d49c7487e597212cca1
AndresTorrico09 <andreslionel09@outlook.com>
2021-07-21 23:05:06 -0300

    set location permissions

--- app/src/main/java/com/distillery/interview/ui/MainActivity.kt
+++ app/src/main/java/com/distillery/interview/ui/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.distillery.interview.ui
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.distillery.interview.R
 import com.distillery.interview.ui.viewpager.WeatherCollectionFragment
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(savedInstanceState == null) {
+        if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.root, WeatherCollectionFragment())
                 .commitAllowingStateLoss()
+        }
+
+        requestLocationPermissions()
+    }
+
+    private fun requestLocationPermissions() {
+        try {
+            if (ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    101
+                )
+            }
+        } catch (e: Exception) {
+            Log.e("reqLocationPermissions", e.message.toString())
         }
     }
 }
