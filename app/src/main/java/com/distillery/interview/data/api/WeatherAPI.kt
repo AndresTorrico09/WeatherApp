2827ef2d890e228854aa9521836904172f478588
AndresTorrico09 <andreslionel09@outlook.com>
2021-07-22 10:48:16 -0300

    change current weather api request (lat/lon)

--- app/src/main/java/com/distillery/interview/data/api/WeatherAPI.kt
+++ app/src/main/java/com/distillery/interview/data/api/WeatherAPI.kt
@@ -8,7 +8,7 @@ import retrofit2.http.GET
 
 interface WeatherAPI {
 
-    @GET("data/2.5/weather?units=metric&q=$LOCATION&appid=$WEATHER_API_KEY")
+    @GET("data/2.5/weather?units=metric&lat=$LAT_BSAS&lon=$LON_BSAS&appid=$WEATHER_API_KEY")
     suspend fun getCurrentWeather(): CurrentWeatherResponse
 
     @GET("data/2.5/onecall?units=metric&lat=$LAT_BSAS&lon=$LON_BSAS&exclude=current,minutely,daily,alerts&appid=$WEATHER_API_KEY")
@@ -20,7 +20,6 @@ interface WeatherAPI {
     companion object {
         const val BASE_URL = "https://api.openweathermap.org"
         const val WEATHER_API_KEY = BuildConfig.WEATHER_API_KEY
-        private const val LOCATION = "Buenos Aires"
         private const val LAT_BSAS = -34.603722
         private const val LON_BSAS = -58.381592
     }
