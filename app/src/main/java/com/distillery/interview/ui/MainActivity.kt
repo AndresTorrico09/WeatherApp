838146b69f790e53ed00ea2e65423f27311fa215
AndresTorrico09 <andreslionel09@outlook.com>
2021-07-23 11:28:56 -0300

    handle permission result, extract request code to const

--- app/src/main/java/com/distillery/interview/ui/MainActivity.kt
+++ app/src/main/java/com/distillery/interview/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.distillery.interview.ui
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.distillery.interview.R
@@ -14,12 +15,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.root, WeatherCollectionFragment())
-                .commitAllowingStateLoss()
-        }
-
         requestLocationPermissions()
     }
 
@@ -30,11 +25,41 @@ class MainActivity : AppCompatActivity() {
             ) {
                 ActivityCompat.requestPermissions(this,
                     arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                    101
+                    REQUEST_CODE
                 )
+            } else {
+                init()
             }
+
         } catch (e: Exception) {
             Log.e("reqLocationPermissions", e.message.toString())
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    init()
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun init() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.root, WeatherCollectionFragment())
+            .commitAllowingStateLoss()
+    }
+
+    companion object {
+        private const val REQUEST_CODE = 1001
     }
 }
